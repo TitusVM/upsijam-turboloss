405ed5f04a8a9a9eb137ac186cc642b5d78ea38e
WindRider97 <benjamin.mouchet@outlook.com>
2022-11-18 23:54:52 +0100

    player score, doesnt update

--- shelf_life/Assets/Scripts/PlayerMovement.cs
+++ shelf_life/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] private float playerSpeed = 10;
+    [SerializeField] private TextMeshProUGUI score;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private int scoreValue;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        scoreValue = 0;
+        score.text = scoreValue.ToString();
     }
 
     // Update is called once per frame
@@ -25,5 +30,15 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position+ movement * playerSpeed * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Collectible")
+        {
+            Destroy(collision.gameObject);
+            scoreValue += 100;
+            score.text = scoreValue.ToString(); 
+        }
     }
 }
