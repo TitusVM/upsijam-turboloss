352dbfd1504300c221007fc5c3ed72ae83b6b51c
WindRider97 <benjamin.mouchet@outlook.com>
2022-11-19 13:44:14 +0100

    new sprites for shopping cart

--- shelf_life/Assets/Scripts/PlayerMovement.cs
+++ shelf_life/Assets/Scripts/PlayerMovement.cs
@@ -6,17 +6,20 @@ public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] private float playerSpeed = 10;
+
     private Animator anim;
     private Vector2 movement;
     private Rigidbody2D rb;
     private Camera cam;
     private Transform rbTransform;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         cam = GetComponent<Camera>();
         rb = GetComponent<Rigidbody2D>();
+
         rbTransform = rb.transform;
         anim= GetComponent<Animator>();
         //Change line below once game is really starting
